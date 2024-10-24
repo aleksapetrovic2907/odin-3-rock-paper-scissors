57d8291fb2579933f37a4257322eceda17fc451e
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-24 17:31:05 +0200

    implement round logic

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,28 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    // If draw.
+    if (humanChoice === computerChoice) {
+        console.log(`It's a draw, you both chose ${humanChoice}!`);
+    }
+    // If player won.
+    else if ((humanChoice == 'rock' && computerChoice == 'scissors')
+        || (humanChoice == 'paper' && computerChoice == 'rock')
+        || (humanChoice == 'scissors' && computerChoice == 'paper')) {
+        console.log(`You've won, ${humanChoice} beats ${computerChoice}!`);
+        humanScore++;
+    }
+    // If player lost.
+    else {
+        console.log(`You've lost, ${computerChoice} beats ${humanChoice}!`);
+        computerScore++;
+    }
+}
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
 
