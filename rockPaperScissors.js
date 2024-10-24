5a5f65da230ff640006c643462c2b6da122d0fa5
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-24 17:20:01 +0200

    implement method for receiving human choice via prompt

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -10,3 +10,13 @@ function getComputerChoice() {
             return 'scissors';
     }
 }
+
+function getHumanChoice() {
+    let choice;
+
+    do {
+        choice = prompt("Choose between rock, paper and scissors.").toLowerCase().trim();
+    } while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors');
+
+    return choice;
+}
